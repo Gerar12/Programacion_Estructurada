64a092f342c34e661b802f2dac2b9df0f494af78
otsuka015 <99282884+otsuka015@users.noreply.github.com>
2022-03-19 20:25:57 -0600

    clase

---
+++ Funciones Divide y vencerás/funsion.c
@@ -0,0 +1,26 @@
+//parametro de una funsion tipo_de_dato > nombre > (parametros)
+
+
+int adition (int a, int b)
+    {
+        int res = a + b;
+        return res;
+
+    }
+
+int main()
+    {
+        printf("Funciones!\n");
+
+
+    int aditoRes = adition(3,4);
+
+    printf("Resultado es: %i", aditoRes );
+
+
+
+        return 0;
+
+
+        
+    }
